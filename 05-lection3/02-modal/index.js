2287f3e9ce62e440c1f8f58b68bfdbccfaed7168
polina.gladysheva <polina.gladysheva@rtkit.ru>
2024-02-07 17:30:31 +0500

    5 task. modal. fix

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,19 +2,16 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
-    const modalOverlay = document.getElementById('modal-overlay');
 
-    if (!close || !modalOpen || !modal || !modalOverlay) {
+    if (!close || !modalOpen || !modal) {
         return;
     }
 
     modalOpen.addEventListener('click', () => {
         modal.style.display = 'block';
-        modalOverlay.style.display = 'block';
     });
 
     close.addEventListener('click', () => {
         modal.style.display = 'none';
-        modalOverlay.style.display = 'none';
     });
 })();
